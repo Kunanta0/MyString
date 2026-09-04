21cf888f9d1da6115b1754f0b3bc1461bf5cf6c4
Kunanta0 <faxim2020@yandex.ru>
2026-09-04 22:13:29 +0300

    Update MyString.h

--- MyString.h
+++ MyString.h
@@ -1,1 +1,10 @@
+#ifndef MYSTRING_H_INCLUDED
+#define MYSTRING_H_INCLUDED
 
+int Puts(const char*);
+size_t Strlen(const char*);
+char* Strcpy(char* , const char* );
+char* Strcat(char*, const char*);
+int Strcmp(const char*, const char*);
+
+#endif
