aa57a3d6ee3833cb4ff54044025434f829f11c37
Kunanta0 <faxim2020@yandex.ru>
2026-09-11 22:17:08 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <math.h>
 #include "MyString.h"
+#include "Sorters.h"
+#include "comparators.h"
 //const char* s0 = "Hello";
 
 int main(void)
@@ -16,7 +18,7 @@ int main(void)
     s1[4] = '\0';
 
     const char s2[] = {'H', 'E', 'L', 'L', 'O', '\0'};
-    Strcat(s1, s2);
+    Strcpy(s1, s2);
     printf("%s\n", s1);*/
 
     /*int Size = Strlen(s1);
@@ -27,6 +29,21 @@ int main(void)
     /*int ans = Strcmp("apples", "apple");
 
     printf("Strcmp returns %d", ans);*/
+
+    int SIZE = 0;
+    scanf("%d", &SIZE);
+
+    int* data = (int *)calloc(SIZE, sizeof(int));
+
+    for (int i = 0;i < SIZE;++i)
+    {
+        scanf("%d", data + i);
+    }
+
+    //BubbleSort(data, SIZE, CompareUp);
+    QuickSort(data, 0, SIZE-1, CompareDown);
+
+    PrintArr(data, SIZE);
 
     return 0;
 }
