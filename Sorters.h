7b6d6728a2622279b6938a760b7316ff56c861a9
Kunanta0 <faxim2020@yandex.ru>
2026-09-20 16:22:42 +0300

    Update Sorters.h

--- Sorters.h
+++ Sorters.h
@@ -1,18 +1,21 @@
 #ifndef SORTERS_H_INCLUDED
 #define SORTERS_H_INCLUDED
 
-void Swap(int*, int*);
-void BubbleSort(int*, size_t, bool (*CompareFunc) (int*, int*));
-void QuickSort(int*, size_t, bool (*CompareFunc) (int*, int*), size_t);
+void Swap(void*, void*);
+void BubbleSort(void*, size_t, size_t, int (*CompareFunc) (const void*, const void*));
+void QuickSort(int*, size_t, bool (*CompareFunc) (const void*, const void*), size_t);
 
-void Swap(int* a1, int* a2)
+void Swap(void* value_1, void* value_2)
 {
-    int temp = *a1;
-    *a1 = *a2;
-    *a2 = temp;
+    int* a1 = (int*) value_1;
+    int* a2 = (int*) value_2;
+
+    int* temp = a1;
+    a1 = a2;
+    a2 = temp;
 }
 
-void QuickSort(int* arr, size_t SIZE, bool (*CompareFunc) (int* a, int* b), size_t lo = 0)
+void QuickSort(int* arr, size_t SIZE, bool (*CompareFunc) (const void* a, const void* b), size_t lo = 0)
 {
     size_t  hi = SIZE - 1;
     if (lo >= hi) return;
@@ -32,18 +35,25 @@ void QuickSort(int* arr, size_t SIZE, bool (*CompareFunc) (int* a, int* b), size
     QuickSort(arr, hi + 1, CompareFunc, i + 1);
 }
 
-void BubbleSort(int* arr, size_t SIZE, bool (*CompareFunc) (int* a, int* b))
+void BubbleSort(void* data, size_t nnums, size_t SIZE, int (*CompareFunc) (const void* a, const void* b))
 {
-    for (size_t n = SIZE - 1;n >= 0;--n)
+    char* arr = (char*) data;
+    for (size_t n = nnums - 1;n >= 0;--n)
     {
         bool IsChanged = false;
         for (size_t i = 0;i < n;++i)
         {
-            if (CompareFunc(arr + i, arr + i + 1))
+            void* temp = malloc(SIZE);
+            void* arr1 = arr + i * SIZE;
+            void* arr2 = arr + (i + 1) * SIZE;
+            if (CompareFunc(arr1, arr2) > 0)
             {
-                Swap(arr + i, arr + i + 1);
+                memcpy(temp, arr1, SIZE);
+                memcpy(arr1, arr2, SIZE);
+                memcpy(arr2, temp, SIZE);
                 IsChanged = true;
             }
+            free(temp);
         }
         if (!IsChanged) return;
     }
