06758ed0b28ca42a655588d6627b9314c7b0e261
Kunanta0 <faxim2020@yandex.ru>
2026-09-20 16:53:33 +0300

    Update comparators.h

--- comparators.h
+++ comparators.h
@@ -24,33 +24,6 @@ int CompareDown(const void* value_a, const void* value_b)
 
 int CompareStrs2(const void* a, const void* b)
 {
-    /*const char* str1 = *(const char**) a;
-    const char* str2 = *(const char**) b;
-
-    size_t max_size = (Strlen(str1) > Strlen(str2)) ? Strlen(str1) : Strlen(str2);
-
-    for (size_t i = 0, j = 0; i < max_size, j < max_size; ++i, ++j)
-    {
-        char ch1 = *(str1 + i);
-        char ch2 = *(str2 + i);
-        while (!(isalpha(ch1)))
-        {
-            ++i;
-            ch1 = *(str1 + i);
-        }
-        while (!(isalpha(ch2)))
-        {
-            ++j;
-            ch2 = *(str2 + j);
-        }
-        int s1 = ch1 = tolower(ch1);
-        int s2 = ch2 = tolower(ch2);
-
-        if (s1 != s2) return s1 - s2;
-    }
-
-    return 0;*/
-
     const char* s1 = *(const char**) a;
     const char* s2 = *(const char**) b;
 
@@ -60,25 +33,43 @@ int CompareStrs2(const void* a, const void* b)
     s1 += i1;
     s2 += i2;
 
-
-    while (*s1 && *s2)
+    while (i1 * i1 + i2 * i2)
     {
-        while(*s1 && !isalpha((unsigned char)*s1)) s1--;
-        while(*s2 && !isalpha((unsigned char)*s2)) s2--;
+        while((i1) && !isalpha((unsigned char)*s1))
+        {
+            --i1;
+            --s1;
+        }
+        while((i2) && !isalpha((unsigned char)*s2))
+        {
+            --i2;
+            --s2;
+        }
 
-        if(!*s1 || !*s2) break;
+        if(i1 + i2 < 2) break;
 
         int c1 = tolower((unsigned char)*s1);
         int c2 = tolower((unsigned char)*s2);
 
         if (c1 != c2) return c1 - c2;
 
-        s1--;
-        s2--;
+        --s1;
+        --i1;
+
+        --s2;
+        --i2;
     }
 
-    while(*s1 && !isalpha((unsigned char)*s1)) s1--;
-    while(*s2 && !isalpha((unsigned char)*s2)) s2--;
+    while((i1) && !isalpha((unsigned char)*s1))
+    {
+        --s1;
+        --i1;
+    }
+    while((i2) && !isalpha((unsigned char)*s2))
+    {
+        --s2;
+        --i2;
+    }
 
     return (unsigned char)* s1 - (unsigned char)* s2;
 }
